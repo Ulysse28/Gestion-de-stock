31c8fd7fa233f8f0acb1c326cc1a4fc97a9d5205
ulysse28 <uvaldenaire@gmail.com>
2021-08-17 17:09:46 +0200

    fin de la feature default products

---
+++ functions.js
@@ -0,0 +1,235 @@
+/*
+    GESTION DE STOCK
+    JAVASCRIPT FUNCTIONS FILE
+    BY ULYSSE VALDENAIRE
+    15/08/2021
+*/
+
+//fonction qui crée un formulaire pour ajouter un produit
+function addProduct(
+  valueID,
+  valueName,
+  valuePrice,
+  valueStockActu,
+  valueStockMin
+) {
+  form.appendChild(Title);
+  form.appendChild(br);
+  form.appendChild(labelType);
+  form.appendChild(radioDefault);
+  form.appendChild(defaultTxt);
+  form.appendChild(radioNormal);
+  form.appendChild(normalTxt);
+  inputID.setAttribute("value", valueID);
+  form.appendChild(inputID);
+  form.appendChild(br5);
+  form.appendChild(labelName);
+  inputName.setAttribute("value", valueName);
+  form.appendChild(inputName);
+  form.appendChild(br1);
+  form.appendChild(labelPrice);
+  inputPrice.setAttribute("value", valuePrice);
+  form.appendChild(inputPrice);
+  form.appendChild(br2);
+  form.appendChild(labelStockActu);
+  inputStockActu.setAttribute("value", valueStockActu);
+  form.appendChild(inputStockActu);
+  form.appendChild(br3);
+  form.appendChild(labelStockMin);
+  inputStockMin.setAttribute("value", valueStockMin);
+  form.appendChild(inputStockMin);
+  form.appendChild(br4);
+  form.appendChild(btnValider);
+  form.appendChild(btnReset);
+  divForm.append(form);
+  sectionOptions.appendChild(divForm);
+}
+
+//Fonction pour ajouter un objet dans le local storage
+function addProductLocalStorage() {
+  let product;
+  if (radioNormal.checked === true) {
+    //on crée un nouvel objet product avec le contenu du formulaire
+    product = new Product(
+      inputID.value,
+      "normal",
+      inputName.value,
+      inputPrice.value,
+      inputStockActu.value,
+      inputStockMin.value
+    );
+  } else {
+    product = new Product(
+      inputID.value,
+      "default",
+      inputName.value,
+      inputPrice.value,
+      0,
+      0
+    );
+  }
+  let listFinale;
+  listProducts = [];
+  listProducts.push(product);
+  listProductsBefore = localStorage.getItem("products");
+  console.log(listProductsBefore);
+  if (listProductsBefore === null) {
+    listFinale = listProducts;
+  } else {
+    listProductsBefore = JSON.parse(listProductsBefore);
+    console.log(listProductsBefore);
+    console.log(listProducts);
+    listFinale = listProductsBefore.concat(listProducts);
+    console.log(listFinale);
+  }
+  localStorage.setItem("products", JSON.stringify(listFinale));
+  createProduct(product);
+}
+
+//fonction qui affiche le produit après l'avoir ajouter
+function createProduct(product) {
+  divProduct.setAttribute("class", "divProduct");
+  let checkboxProduct = document.createElement("input");
+  checkboxProduct.setAttribute("type", "checkbox");
+  checkboxProduct.setAttribute("class", "checkboxesInput");
+  checkboxProduct.setAttribute("name", product.name);
+  divProduct.appendChild(checkboxProduct);
+  let productLine = document.createElement("label");
+  if (product.type === "normal") {
+    productLine.textContent =
+      product.name +
+      " prix : " +
+      product.price +
+      " stock actuelle : " +
+      product.stockActu +
+      " stock minimun : " +
+      product.stockMin;
+  } else {
+    productLine.textContent = product.name + " prix : " + product.price;
+  }
+  productLine.setAttribute("for", product.name);
+  productLine.setAttribute("class", "products");
+  divProduct.appendChild(productLine);
+  const br = document.createElement("br");
+  divProduct.appendChild(br);
+  sectionProducts.appendChild(divProduct);
+}
+
+//Fonction pour supprimer un produit en particulier
+function deleteProduct(add) {
+  let allCheckboxesProducts =
+    document.getElementsByClassName("checkboxesInput");
+  let productToModify;
+  let checkbox;
+  let productLine;
+  let index;
+  let checkedBox = [];
+  for (let i = 0; i < allCheckboxesProducts.length; i++) {
+    if (allCheckboxesProducts[i].checked === true) {
+      checkedBox.push(allCheckboxesProducts[i]);
+    }
+  }
+  console.log(checkedBox.length);
+  if (checkedBox.length != 0) {
+    for (i = 0; i < allCheckboxesProducts.length; i++) {
+      if (allCheckboxesProducts[i].checked === true) {
+        productToModify = allCheckboxesProducts[i].name;
+        checkbox = allCheckboxesProducts[i];
+        console.log(checkbox.name);
+        index = i;
+      }
+    }
+    let products = document.getElementsByClassName("products");
+    let listProducts = localStorage.getItem("products");
+    listProducts = JSON.parse(listProducts);
+    for (let i = 0; i < listProducts.length; i++) {
+      if (listProducts[i].name === checkbox.name) {
+        if (add === true) {
+          addProduct(
+            listProducts[i].id,
+            listProducts[i].name,
+            listProducts[i].price
+          );
+        }
+        listProducts.splice(i, 1);
+        localStorage.setItem("products", JSON.stringify(listProducts));
+      }
+    }
+    divProduct.removeChild(checkbox);
+    divProduct.removeChild(products[index]);
+  } else {
+    alert("Vous devez selectionner un produit !");
+  }
+}
+
+//Fonction qui crée la liste (array) de la liste de course
+function createShopList() {
+  let listProduct = localStorage.getItem("products");
+  if(listProduct === null){
+    alert("Il n'y a pas de produit enregistré");
+  }else{
+    listProduct = JSON.parse(listProduct);
+    console.log(listProduct);
+    let listProductsToBuy = [];
+    for (let i = 0; i < listProduct.length; i++) {
+      if (
+        listProduct[i].type === "normal" &&
+        listProduct[i].stockActu <= listProduct[i].stockMin
+      ) {
+        listProductsToBuy.push(listProduct[i]);
+      } else if (listProduct[i].type === "default") {
+        listProductsToBuy.push(listProduct[i]);
+      }
+    }
+    console.log(listProductsToBuy);
+    let totalPrice = 0;
+    for (product of listProductsToBuy) {
+      createElementList(product);
+      let number = product.stockMin - product.stockActu;
+      if (number === 0) {
+        number = 1;
+      }
+      product.price = Number(product.price);
+      console.log(totalPrice);
+      totalPrice += product.price * number;
+      console.log(totalPrice);
+    }
+   /*
+    let totalPriceElement = document.createElement("p");
+    totalPriceElement.textContent = totalPrice;
+    console.log(totalPrice);
+    
+    divList.appendChild(totalPriceElement);
+  */
+  }
+}
+
+//Function qui crée la liste de course en html
+function createElementList(product) {
+  const name = product.name;
+  const price = product.price;
+  const radioProduct = document.createElement("input");
+  radioProduct.setAttribute("type", "checkbox");
+  radioProduct.setAttribute("name", product.id);
+  radioProduct.setAttribute("class", "productList");
+  divList.appendChild(radioProduct);
+  const productToBuy = document.createElement("label");
+  if (product.type === "normal") {
+    let number = product.stockMin - product.stockActu;
+    if (number === 0) {
+      number = 1;
+    }
+    productToBuy.textContent = number + "  " + name + " prix : " + price + " €";
+  } else {
+    productToBuy.textContent = name + "  " + price;
+  }
+  divList.appendChild(productToBuy);
+
+
+
+ 
+
+
+  sectionProducts.appendChild(divList);
+ 
+}
--- script.js
+++ script.js
@@ -11,12 +11,24 @@ let btnModifier = document.getElementById("modifier");
 const sectionProducts = document.getElementById("products");
 let sectionOptions = document.getElementById("options");
 let divProduct = document.createElement("div");
-
 //Variables pour le formulaires
 const divForm = document.createElement("div");
 const form = document.createElement("form");
 const Title = document.createElement("h3");
 Title.textContent = "Ajouter/Modifier un produit";
+const labelType = document.createElement("label");
+labelType.textContent = "Type de produit";
+const radioDefault = document.createElement("input");
+radioDefault.setAttribute("type", "radio");
+radioDefault.setAttribute("name", "question");
+radioDefault.setAttribute("id", "radioDefault");
+const defaultTxt = document.createElement("label");
+defaultTxt.textContent = "default";
+const radioNormal = document.createElement("input");
+radioNormal.setAttribute("type", "radio");
+radioNormal.setAttribute("name", "question");
+const normalTxt = document.createElement("label");
+normalTxt.textContent = "Normal";
 const inputID = document.createElement("input");
 inputID.setAttribute("type", "number");
 const labelName = document.createElement("label");
@@ -37,6 +49,10 @@ const inputStockMin = document.createElement("input");
 inputStockMin.setAttribute("type", "number");
 const btnValider = document.createElement("button");
 btnValider.textContent = "valider";
+const btnReset = document.createElement('button');
+btnReset.textContent = 'Cacher le formualire';
+
+
 let br = document.createElement("br");
 const br1 = document.createElement("br");
 const br2 = document.createElement("br");
@@ -45,12 +61,17 @@ let br4 = document.createElement("br");
 const br3 = document.createElement("br");
 const divList = document.createElement("div");
 
+
+
+
+
 //array des produits
 listProducts = [];
 
 //constructeur de l'objet product
-function Product(id, name, price, stockActu, stockMin) {
+function Product(id, type, name, price, stockActu, stockMin) {
   this.id = id;
+  this.type = type;
   this.name = name;
   this.price = price;
   this.stockActu = stockActu;
@@ -77,98 +98,34 @@ btnAjouter.addEventListener("click", function () {
   addProduct(0, "", 0, 0, 0);
 });
 
-//au click sur le bouton valider
+//au click sur le bouton valider --> l'objet est envoyé dans le local storage
 btnValider.addEventListener("click", function (event) {
   event.preventDefault();
-  //on crée un nouvel objet product avec le contenu du formulaire
-  let product = new Product(
-    inputID.value,
-    inputName.value,
-    inputPrice.value,
-    inputStockActu.value,
-    inputStockMin.value
-  );
-  let listFinale;
-  listProducts = [];
-  listProducts.push(product);
-  listProductsBefore = localStorage.getItem("products");
-  console.log(listProductsBefore);
-  if (listProductsBefore === null) {
-    listFinale = listProducts;
-  } else {
-    listProductsBefore = JSON.parse(listProductsBefore);
-    console.log(listProductsBefore);
-    console.log(listProducts);
-    listFinale = listProductsBefore.concat(listProducts);
-    console.log(listFinale);
-  }
-  localStorage.setItem("products", JSON.stringify(listFinale));
-  createProduct(product);
+  addProductLocalStorage(); 
 });
 
-//fonction qui crée un formulaire pour ajouter un produit
-function addProduct(
-  valueID,
-  valueName,
-  valuePrice,
-  valueStockActu,
-  valueStockMin
-) {
-  form.appendChild(Title);
-  form.appendChild(br);
-  inputID.setAttribute("value", valueID);
-  form.appendChild(inputID);
-  form.appendChild(br5);
-  form.appendChild(labelName);
-  inputName.setAttribute("value", valueName);
-  form.appendChild(inputName);
-  form.appendChild(br1);
-  form.appendChild(labelPrice);
-  inputPrice.setAttribute("value", valuePrice);
-  form.appendChild(inputPrice);
-  form.appendChild(br2);
-  form.appendChild(labelStockActu);
-  inputStockActu.setAttribute("value", valueStockActu);
-  form.appendChild(inputStockActu);
-  form.appendChild(br3);
-  form.appendChild(labelStockMin);
-  inputStockMin.setAttribute("value", valueStockMin);
-  form.appendChild(inputStockMin);
-  form.appendChild(br4);
-  form.appendChild(btnValider);
-  divForm.append(form);
-  sectionOptions.appendChild(divForm);
-}
+//Boutton radio pour les produits par defaults, les input Stock sont désactivés
+radioDefault.addEventListener("change", function () {
+  if (this.checked === true) {
+    inputStockActu.disabled = true;
+    inputStockMin.disabled = true;
+  }
+});
 
-//fonction qui affiche le produit après l'avoir ajouter
-function createProduct(product) {
-  divProduct.setAttribute("class", "divProduct");
-  let checkboxProduct = document.createElement("input");
-  checkboxProduct.setAttribute("type", "checkbox");
-  checkboxProduct.setAttribute("class", "checkboxesInput");
-  checkboxProduct.setAttribute("name", product.name);
-  divProduct.appendChild(checkboxProduct);
-  let productLine = document.createElement("label");
-  productLine.textContent =
-    product.name +
-    " prix : " +
-    product.price +
-    " stock actuelle : " +
-    product.stockActu +
-    " stock minimun : " +
-    product.stockMin;
-  productLine.setAttribute("for", product.name);
-  productLine.setAttribute("class", "products");
-  divProduct.appendChild(productLine);
-  const br = document.createElement("br");
-  divProduct.appendChild(br);
-  sectionProducts.appendChild(divProduct);
-}
+//Boutton radio pour les produits normaux, les input stock sont activés
+radioNormal.addEventListener("change", function () {
+  if (this.checked == true) {
+    inputStockActu.disabled = false;
+    inputStockMin.disabled = false;
+  }
+});
 
 //supprimer tout le contenu du local storage
 document.getElementById("deleteAll").addEventListener("click", function () {
+
   localStorage.clear();
   divProduct.textContent = "";
+  alert('Tous les produits ont été supprimés !');
 });
 
 //Modifier un produit
@@ -176,92 +133,59 @@ btnModifier.addEventListener("click", function () {
   deleteProduct(true);
 });
 
+//Supprimer un produit
 let btnDelete = document.getElementById("delete");
 btnDelete.addEventListener("click", function () {
   deleteProduct(false);
 });
 
-function deleteProduct(add) {
-  let allCheckboxesProducts =
-    document.getElementsByClassName("checkboxesInput");
-  let productToModify;
-  let checkbox;
-  let productLine;
-  let index;
-  for (i = 0; i < allCheckboxesProducts.length; i++) {
-    if (allCheckboxesProducts[i].checked === true) {
-      productToModify = allCheckboxesProducts[i].name;
-      checkbox = allCheckboxesProducts[i];
-      console.log(checkbox.name);
-      index = i;
-    }
-  }
-  let products = document.getElementsByClassName("products");
-  let listProducts = localStorage.getItem("products");
-  listProducts = JSON.parse(listProducts);
-  for (let i = 0; i < listProducts.length; i++) {
-    if (listProducts[i].name === checkbox.name) {
-      if (add === true) {
-        addProduct(
-          listProducts[i].id,
-          listProducts[i].name,
-          listProducts[i].price
-        );
-      }
+//Cacher le formulaire
+btnReset.addEventListener('click', function(){
+  sectionOptions.removeChild(divForm);
+})
 
-      listProducts.splice(i, 1);
-      localStorage.setItem("products", JSON.stringify(listProducts));
-    }
-  }
-  divProduct.removeChild(checkbox);
-  divProduct.removeChild(products[index]);
-}
-
+//Afficher la liste de courses
 let btnShoppingListe = document.getElementById("shoppingList");
-
 btnShoppingListe.addEventListener("click", function () {
   console.log("hello");
   createShopList();
+  sectionProducts.removeChild(divProduct);
 });
 
-function createShopList() {
-  let listProduct = localStorage.getItem("products");
-  listProduct = JSON.parse(listProduct);
-  console.log(listProduct);
-  let listProductsToBuy = [];
-  for (let i = 0; i < listProduct.length; i++) {
-    if (listProduct[i].stockActu <= listProduct[i].stockMin) {
-      listProductsToBuy.push(listProduct[i]);
-    }
-  }
-  console.log(listProductsToBuy);
-  let totalPrice = 0;
-  for (product of listProductsToBuy) {
-    createElementList(product);
-    let number = product.stockMin - product.stockActu;
-    if (number === 0) {
-      number = 1;
-    }
-    product.price = Number(product.price);
-    console.log(totalPrice);
-    totalPrice += product.price * number;
-    console.log(totalPrice);
-  }
-  let totalPriceElement = document.createElement("p");
-  totalPriceElement.textContent = totalPrice;
-  console.log(totalPrice);
-  divList.appendChild(totalPriceElement);
-}
+document.getElementById('list').addEventListener('click', function(){
 
-function createElementList(product) {
-  const name = product.name;
-  let number = product.stockMin - product.stockActu;
-  if (number === 0) {
-    number = 1;
+  divProduct.remove(divList);
+  let liste = document.getElementsByClassName('productList');
+  console.log(liste);
+  let listProduct = localStorage.getItem('products');
+  listProduct =   JSON.parse(listProduct);
+  console.log(listProduct);
+  let finalList = [];
+  let finalPrice = 0;
+  for(let i = 0; i < liste.length; i++){
+    for(let j = 0; j < listProduct.length; j++){
+      if(liste[i].name === listProduct[j].id && liste[i].checked === true){
+        console.log(listProduct[j]);
+        finalList.push(listProduct[j])
+      }
+    }
   }
-  const price = product.price;
-  const productToBuy = document.createElement("p");
-  productToBuy.textContent = number + "  " + name + " prix : " + price + " €";
-  divList.appendChild(productToBuy);
-  divProduct.appendChild(divList);
-}
+
+  const title = document.createElement('p');
+  title.textContent = "Liste de course";
+
+  divList.appendChild(title);
+
+
+  for(product of finalList){
+    createElementList(product);
+    finalPrice = finalPrice + Number(product.price);
+  }
+
+ 
+
+
+
+
+  console.log(finalPrice);
+})
